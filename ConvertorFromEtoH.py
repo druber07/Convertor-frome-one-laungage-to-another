9e9f02dc23d938d3a55d63e1ec3f1bec48483dd1
druber07 <druber07program@gmail.com>
2020-04-22 20:22:20 +0300

    Update

--- ConvertorFromEtoH.py
+++ ConvertorFromEtoH.py
@@ -1,5 +1,5 @@
 hebrew = "/'קראטוןםפשדגכעיחלךף,זסבהנמצתץ. "
-english = "qwertyuiopasdfghjkl;'zxcvbnm,./ "
+english = "qwertyuiopasdfghjkl;'zxcvbnm,./ S"
 
 def convHeEn(msg):
     msgN = ''
@@ -15,10 +15,61 @@ def convEnHe(msg):
         msgN += hebrew[english.index(char)]
 
     return msgN[::1]
-# The checker is probably the problem of the program  
+
+def launguage(msg):
+    ask = True
+
+    def Eng(msg):
+        for let in msg:
+            letterIf = True
+            for LEng in english:
+                if let != LEng:
+                    letterIf = False
+                else:
+                    letterIf = True
+
+            if  not letterIf:
+                return False
+        return True
+
+    def Rus(msg):
+        for let in msg:
+            letterIf = True
+            for LEng in english:
+                if let != LEng:
+                    letterIf = False
+                else:
+                    letterIf = True
+
+            if  not letterIf:
+                return False
+        return True
+        
+    def Heb(msg):
+        for let in msg:
+            letterIf = True
+            for LEng in english:
+                if let != LEng:
+                    letterIf = False
+                else:
+                    letterIf = True
+
+            if  not letterIf:
+                return False
+        return True
+        
+        
+    if Eng(msg):
+        print('That is english')
+    elif Heb(msg):
+        print('That is hebrew')
+
 if __name__ == '__main__':
-    msg = input("Enter message to convert: ")
-    if(msg[-1] == x for x in english):
+    msg = input("Enter message to convert:\n")
+    launguage(msg)
+
+"""""
+if(msg[-1] == x for x in english):
         print("That's english")
         msgN = ''
         for char in msg:
@@ -30,5 +81,4 @@ if __name__ == '__main__':
         msgN = ''
         for char in msg:
             msgN += english[hebrew.index(char)]
-
-        print(msgN)
+"""
